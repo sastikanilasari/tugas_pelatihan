046dde6922a43dec64f23063647a50dd1af101a4
Sastika Nilasari <sastikanila123@gmail.com>
2022-08-31 23:48:26 +0700

    Menambahakan bar dan mengatur ukurannya

--- tugas_sastika.py
+++ tugas_sastika.py
@@ -14,3 +14,10 @@ import numpy as np
 labels = ['Matematika','Fisika', 'Kimia', 'Biologi','Bahasa Inggris', 'Bahasa Indonesia']
 men_means = [80, 82, 79, 81, 85, 89]
 women_means = [79, 83, 78, 85, 88, 88]
+
+x = np.arange(len(labels)) #lokasi label tiap mata pelajaran
+width = 0.35 #lebar bar
+
+fig, ax = plt.subplots()
+rects1 = ax.bar(x - width/2, men_means, width, label = 'Men')
+rects2 = ax.bar(x + width/2, women_means, width, label = 'Women')
