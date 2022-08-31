7609a51589c1425b2e0bd867f9f0989678fce185
Sastika Nilasari <sastikanila123@gmail.com>
2022-08-31 23:41:59 +0700

    menambahkan library dan data

--- tugas_sastika.py
+++ tugas_sastika.py
@@ -3,3 +3,14 @@ Identitas:
 Nama: Sastika Nilasari
 Email: sastikanila123@gmail.com
 """
+
+#impor library plot
+from matplotlib import pyplot as plt
+
+#impor untuk numpy
+import numpy as np
+
+#data dasar
+labels = ['Matematika','Fisika', 'Kimia', 'Biologi','Bahasa Inggris', 'Bahasa Indonesia']
+men_means = [80, 82, 79, 81, 85, 89]
+women_means = [79, 83, 78, 85, 88, 88]
