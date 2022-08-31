171385026e1757781cafac6aef1fcd08a8b9e4fe
Sastika Nilasari <sastikanila123@gmail.com>
2022-08-31 23:50:59 +0700

    menambahkan text dan legenda pada grafik

--- tugas_sastika.py
+++ tugas_sastika.py
@@ -21,3 +21,16 @@ width = 0.35 #lebar bar
 fig, ax = plt.subplots()
 rects1 = ax.bar(x - width/2, men_means, width, label = 'Men')
 rects2 = ax.bar(x + width/2, women_means, width, label = 'Women')
+
+#Menambhakan text pada label, judul, dan legenda
+ax.set_ylabel('Nilai')
+ax.set_title('Nilai berdasarkan Mata Pelajaran dan Jenis Kelamin')
+ax.set_xticks(x, labels)
+ax.legend()
+
+ax.bar_label(rects1, padding = 3)
+ax.bar_label(rects2, padding = 3)
+
+fig.tight_layout()
+
+plt.show()
